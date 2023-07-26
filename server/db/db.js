8338461068618048c1625339f21c19875913e6b1
Alejandro Armas <lxarmas@gmail.com>
2023-07-26 03:06:36 +0300

    Configure DATABASE_URL_2

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,17 @@
-const Sequelize = require('sequelize')
-const pkg = require('../../package.json')
+const Sequelize = require('sequelize');
+const pkg = require('../../package.json');
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
 
 const config = {
   logging: false
 };
 
-if(process.env.LOGGING === 'true'){
-  delete config.logging
+if (process.env.LOGGING === 'true') {
+  delete config.logging;
 }
 
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE_URL){
+if (process.env.DATABASE_URL) {
   config.dialectOptions = {
     ssl: {
       rejectUnauthorized: false
@@ -20,6 +19,29 @@ if(process.env.DATABASE_URL){
   };
 }
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
-module.exports = db
+let db;
+if (process.env.DATABASE_URL) {
+  // Parse the DATABASE_URL to extract the protocol
+  const urlParts = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+  if (urlParts) {
+    const [, username, password, host, port, database] = urlParts;
+    db = new Sequelize(database, username, password, {
+      host,
+      port,
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    });
+  } else {
+    console.error('Invalid DATABASE_URL');
+    process.exit(1); // Exit the process if the URL format is invalid
+  }
+} else {
+  db = new Sequelize(`postgres://localhost:5432/${databaseName}`, config);
+}
+
+module.exports = db;
